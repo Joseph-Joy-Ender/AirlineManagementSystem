ab07a427c200b741c1c3c3603e5be2260b90046b
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-03-17 23:50:18 +0100

    "controller test"

---
+++ src/main/java/com/example/airlineproject/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.airlineproject.controller;
+
+import com.example.airlineproject.dtos.request.UserRegisterRequest;
+import com.example.airlineproject.exceptions.UserException;
+import com.example.airlineproject.services.UserService;
+import com.example.airlineproject.utils.ApiResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/user")
+@CrossOrigin("*")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> registerUser(@RequestBody UserRegisterRequest registerRequest) throws UserException {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(userService.register(registerRequest));
+    }
+}
